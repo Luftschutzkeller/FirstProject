907e1ea27df741fe69a68a58ce09bda194d1ae19
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2021-09-08 23:49:33 +0300

    Паше

---
+++ app/src/main/java/ru/itmo/firstproject/MainActivity.kt
@@ -0,0 +1,30 @@
+package ru.itmo.firstproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            openSecondActivity()
+        }
+
+        }
+
+
+
+
+
+    private fun openSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+
+    }
+}
